88a7747bc0e225e50fad0913cf4582e287df23ff
mudasirking <165293870+SahilKing71@users.noreply.github.com>
2025-09-27 14:53:31 +0500

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -13,9 +13,9 @@ async (conn, mek, m, { from, sender, reply }) => {
     try {
         // Owner information (you can modify these values)
         const ownerInfo = {
-            name: "SHAHID-XMD",
-            number: "+923423706291",
-            photo: "https://files.catbox.moe/sxa61f.jpg",
+            name: "MUDASIR_MS",
+            number: "+923239956476",
+            photo: "https://qu.ax/wKRmV.jpg",
             bio: "The creator of this amazing bot"
         };
 
@@ -48,7 +48,7 @@ async (conn, mek, m, { from, sender, reply }) => {
     } catch (e) {
         console.error("Creator Command Error:", e);
         // Fallback text if image fails
-        await reply(`👑 *Creator Info*\n\nName: SHAHID-XMD\nNumber: +923423706291\n\nContact for bot support!`);
+        await reply(`👑 *Creator Info*\n\nName: MUDASIR_MS\nNumber: +923239956476\n\nContact for bot support!`);
     }
 });
 
